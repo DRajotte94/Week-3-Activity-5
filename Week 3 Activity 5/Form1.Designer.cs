6e4ee5e02513e12aa5b38abfeda47c480e0dc9d6
drajo <drajo@DESKTOP-LSTFBT7>
2021-10-10 11:41:10 -0700

    Add project files.

---
+++ Week 3 Activity 5/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace Week_3_Activity_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.fileNameText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.browse = new System.Windows.Forms.Button();
+            this.displayBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lowerCase = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.longWord = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mostVowels = new System.Windows.Forms.TextBox();
+            this.lastWord = new System.Windows.Forms.TextBox();
+            this.firstWord = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // fileNameText
+            // 
+            this.fileNameText.BackColor = System.Drawing.SystemColors.Window;
+            this.fileNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileNameText.Location = new System.Drawing.Point(28, 35);
+            this.fileNameText.Name = "fileNameText";
+            this.fileNameText.Size = new System.Drawing.Size(498, 24);
+            this.fileNameText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File name";
+            // 
+            // browse
+            // 
+            this.browse.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browse.Location = new System.Drawing.Point(543, 35);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(107, 26);
+            this.browse.TabIndex = 2;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = false;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // displayBox
+            // 
+            this.displayBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayBox.Location = new System.Drawing.Point(28, 96);
+            this.displayBox.Multiline = true;
+            this.displayBox.Name = "displayBox";
+            this.displayBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.displayBox.Size = new System.Drawing.Size(622, 149);
+            this.displayBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Original File Content";
+            // 
+            // lowerCase
+            // 
+            this.lowerCase.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lowerCase.Location = new System.Drawing.Point(28, 276);
+            this.lowerCase.Multiline = true;
+            this.lowerCase.Name = "lowerCase";
+            this.lowerCase.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.lowerCase.Size = new System.Drawing.Size(622, 149);
+            this.lowerCase.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lowercase";
+            // 
+            // longWord
+            // 
+            this.longWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.longWord.Location = new System.Drawing.Point(28, 460);
+            this.longWord.Name = "longWord";
+            this.longWord.Size = new System.Drawing.Size(122, 24);
+            this.longWord.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 441);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Longest Word";
+            // 
+            // mostVowels
+            // 
+            this.mostVowels.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mostVowels.Location = new System.Drawing.Point(528, 460);
+            this.mostVowels.Name = "mostVowels";
+            this.mostVowels.Size = new System.Drawing.Size(122, 24);
+            this.mostVowels.TabIndex = 9;
+            // 
+            // lastWord
+            // 
+            this.lastWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastWord.Location = new System.Drawing.Point(365, 460);
+            this.lastWord.Name = "lastWord";
+            this.lastWord.Size = new System.Drawing.Size(122, 24);
+            this.lastWord.TabIndex = 10;
+            // 
+            // firstWord
+            // 
+            this.firstWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstWord.Location = new System.Drawing.Point(196, 460);
+            this.firstWord.Name = "firstWord";
+            this.firstWord.Size = new System.Drawing.Size(122, 24);
+            this.firstWord.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(525, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Most Vowels";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(362, 441);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Last Alphabetically";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(193, 441);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "First Alphabetically";
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.Location = new System.Drawing.Point(543, 508);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(107, 26);
+            this.save.TabIndex = 15;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(683, 560);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.firstWord);
+            this.Controls.Add(this.lastWord);
+            this.Controls.Add(this.mostVowels);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.longWord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lowerCase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.displayBox);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileNameText);
+            this.Name = "Form1";
+            this.Text = "Using Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.TextBox fileNameText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.TextBox displayBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lowerCase;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox longWord;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox mostVowels;
+        private System.Windows.Forms.TextBox lastWord;
+        private System.Windows.Forms.TextBox firstWord;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+    }
+}
+
---
+++ Week 3 Activity 5/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Week_3_Activity_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, EventArgs e) // browse button click
+        {
+            openFile.InitialDirectory = @"C:\"; // Sets files search directory
+            openFile.Title = "Browse Files"; // title of file search form
+
+            // if user selects a file and clicks OK
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                fileNameText.Text = openFile.FileName; // diplay file name in textbox
+                displayBox.Clear(); // if textbox already contains input from previous file, clear contents
+
+                String line; // variable for each line within text file
+
+                StreamReader inputFile;
+                inputFile = File.OpenText(openFile.FileName);
+                
+                while (!inputFile.EndOfStream) // breaks at end of file
+                {
+                    line = inputFile.ReadLine(); // reads each line of text file and saves it to line variable
+
+                    // credit to user1693593 on https://stackoverflow.com/questions/13318561/adding-new-line-of-data-to-textbox
+                    displayBox.AppendText(line); // adds data to textbox
+                    displayBox.AppendText(Environment.NewLine); // creates a new line for next loop
+                }
+
+                inputFile.Close(); // close the file
+
+                String lower; // string variable for converting to lowercase
+                lower = displayBox.Text.ToLower(); // saves textbox content from original file data to new variable and converts to lowercase
+                lowerCase.Text = lower; // displays content in new textbox
+
+                String[] words = lower.Split(new[] { " " }, StringSplitOptions.None); // splits text into an array
+
+                // getting first and last alphabetically 
+                Array.Sort(words); // sorts the array into alphabetical order
+                // Takes the first and last words from the newly sorted array
+                String first = words[0];
+                String last = words[words.Length - 1];
+                // displays results
+                firstWord.Text = first;
+                lastWord.Text = last;
+
+                // getting the longest word
+                String longest = ""; // variable being collected with for loop
+                int count = 0; // integer used for comparisons
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i].Length > count) // uses count to hold length value of longest word, compares it against the length of currently indexed word
+                    {
+                        longest = words[i];
+                        count = longest.Length;
+                        longWord.Text = longest; // displays new data
+                    }
+                }
+
+                // getting word with the most vowels
+                // credit Reed Copsey from  https://stackoverflow.com/questions/18109890/c-sharp-count-vowels
+                var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' }; // list of vowels for comparison
+                int total = 0;
+                for (int i = 0; i < words.Length; i++)
+                {
+                    int x = 0; // count for number of values in each word
+                    String checkWord = words[i]; // saves current word to variale
+                    for (int j = 0; j < checkWord.Length; j++)
+                    {
+                        if (vowels.Contains(checkWord[j])) // indexes each letter of the word and checks if it contains a value
+                        {
+                            x++; // increments the counter each time a letter is a vowel
+                        }
+                        if (x > total) // if number of vowels in current word exceeds previous highest total
+                        {
+                            total = x; // new highest total
+                            mostVowels.Text = words[i]; // displays the word
+                        }
+                    } 
+                }
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            saveFile.InitialDirectory = @"C:\"; // sets files save directory
+            saveFile.Title = "Save File"; // creates title for file window
+
+            // if user inputs a name and selects a file save location then presses ok
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter outputFile;
+                outputFile = File.CreateText(saveFile.FileName);
+
+                // write data to new text file
+                outputFile.WriteLine("Original file contents: \n" + displayBox.Text);
+                outputFile.WriteLine("Lowercase: \n" + lowerCase.Text);
+                outputFile.WriteLine("Longest word: " + longWord.Text);
+                outputFile.WriteLine("First alphabetically: " + firstWord.Text);
+                outputFile.WriteLine("Last alphabetically: " + lastWord.Text);
+                outputFile.WriteLine("Most vowels: " + mostVowels.Text);
+
+                outputFile.Close(); // close file
+
+            }
+        }
+    }
+}
